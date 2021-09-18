5001899627b42a45683a00dd7128155c2091e9c2
Jimmy Madon <hello@jimmymadon.com>
2021-09-18 22:53:32 +0100

    8 - Add nprogress loading bar and style to _app

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,11 @@
+import Router from 'next/router';
+import nProgress from 'nprogress';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
