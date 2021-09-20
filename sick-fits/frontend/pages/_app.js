3d88deb1726529762070e13f8184f0c2b89d7c9a
Jimmy Madon <hello@jimmymadon.com>
2021-09-20 11:28:35 +0100

    19 - Setup Apollo Client

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -2,15 +2,29 @@ import Router from 'next/router';
 import nProgress from 'nprogress';
 import Page from '../components/Page';
 import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => nProgress.start());
 Router.events.on('routeChangeComplete', () => nProgress.done());
 Router.events.on('routeChangeError', () => nProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
+// The apollo prop is passed into MyApp by the withData() function
+function MyApp({ Component, pageProps, apollo }) {
+  console.log(apollo);
   return (
     <Page>
       <Component {...pageProps} />
     </Page>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
